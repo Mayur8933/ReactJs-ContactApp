173d31c83712c3746b078c4806de0c6f62d4792a
Mayur8933 <mp326395@gmail.com>
2021-03-23 17:23:22 +0530

    Adding Project files

---
+++ src/utils/config.js
@@ -0,0 +1,18 @@
+//TODO: DONE add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyD2mpS49K31apqtFdNKAEQFOxg_gHMzGwc",
+  authDomain: "mygitapp-f7ee5.firebaseapp.com",
+  databaseURL: "https://mygitapp-f7ee5-default-rtdb.firebaseio.com",
+  projectId: "mygitapp-f7ee5",
+  storageBucket: "mygitapp-f7ee5.appspot.com",
+  messagingSenderId: "346026448263",
+  appId: "1:346026448263:web:b5b65c251438e9495a2f22",
+  measurementId: "G-4E8LMN4ZVL"
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
